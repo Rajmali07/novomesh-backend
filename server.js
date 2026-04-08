7d5000d80e3cf5ed96116e5047daa129fd225cb7
Bihari Mali <bihari.mali2023@ssipmt.com>
2026-04-08 11:27:48 +0530

    Add new CORS origins for deployment
    by this change i can  access my backend using aws

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.use(cors({
   origin: [
   "http://127.0.0.1:5500",
   "http://localhost:5500",
-  "https://swachraipur.netlify.app"
+  "https://swachraipur.netlify.app",
+  "https://main.d1t7oc4snqavd.amplifyapp.com"
 ],
   methods: ["GET","POST","PUT","DELETE"],
   credentials: true
